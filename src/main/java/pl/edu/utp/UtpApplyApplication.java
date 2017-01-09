8a0cccf7729c72db2d553d5fe36ee555b15912ad
Bartosz Szydzik <bartosz.szydzik@gmail.com>
2017-01-09 11:05:45 +0100

    Initial commit

---
+++ src/main/java/pl/edu/utp/UtpApplyApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.utp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtpApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtpApplyApplication.class, args);
+	}
+}
